757a4f7ce0fc1fdeaebfd825aa2eb9b21cc67837
ldv432 <ldv432@gmail.com>
2024-01-23 11:56:30 -0700

    new stuff

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {});
 
 const apiUrl = "https://api.open-meteo.com/v1/forecast"
-// const wxUrlVariables = "current=temperature_2m,relative_humidity_2m,is_day,weather_code,wind_speed_10m,wind_direction_10m,wind_gusts_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,wind_speed_10m_max,wind_gusts_10m_max"
-const wxUrlVariables = "current=temperature_2m,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min"
+const wxUrlVariables = "current=temperature_2m,relative_humidity_2m,is_day,weather_code,wind_speed_10m,wind_direction_10m,wind_gusts_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,wind_speed_10m_max,wind_gusts_10m_max"
+// const wxUrlVariables = "current=temperature_2m,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min"
 const locations = [
   { name: 'Denver', coords: "latitude=39.7392&longitude=-104.9847" },
   { name: 'Colorado Springs', coords: "latitude=38.8339&longitude=-104.8214" },
@@ -19,20 +19,27 @@ const wxFetch = (location) => {
   .then(data => printData(data))
   // .then(data => console.log(data))
 }
-// locations.map(location => wxFetch(location));
+locations.map(location => wxFetch(location));
 
 
 function printData(data){
   //declare currents
-  const citiesCurrents = {
-    
-  }
   const currentData = data.current
   const currentTemp = currentData.temperature_2m
   const currentWind = currentData.wind_speed_10m
   const currentWeather = currentData.weather_code
   const currentCard = document.getElementById("currentCard")
   console.log(currentTemp)
+
+  const citiesCurrents = {
+    "Denver": [currentTemp, currentWind, currentWeather],
+    "Colorado Springs": [currentTemp, currentWind, currentWeather],
+    "Pueblo": [currentTemp, currentWind, currentWeather],
+    "Fort Collins": [currentTemp, currentWind, currentWeather],
+    "Grand Junction": [currentTemp, currentWind, currentWeather]
+  }
+
+  console.log(citiesCurrents)
 
 
   
@@ -45,6 +52,6 @@ function printData(data){
  
 
   
-  console.log(forecastHighs)
+
 }
 
