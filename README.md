2768ae6598a97f453dcb9f956e1ebcd097db5f90
ldv432 <ldv432@gmail.com>
2024-02-12 12:19:43 -0700

    added a README

--- README.md
+++ README.md
@@ -1,17 +1,10 @@
 # THE BORING COLORADO WEATHER APP
 
-Simple weather app that displays current weather information for 5 cities in Colorado.
+The Boring Colorado Weather App is simple take on the modern day weather app on your phones. 
 
 ## Description
 
-Not much to this app other than clicking a button and seeing some stuff about the weather along with temperatures and wind data.
+This app uses 5 buttons designated to 5 cities fetch'd from a local db.json database. The data is static so you'll always get the same weather information for each city. The user will click different buttons to get data for different cities. The use of this app is fairly straightforward.
 
 ## Authors
-
-Contributors names and contact info
-
-ex. Luke Victor
-
-## Acknowledgments
-
-God and Jesus
+Luke Victor (Jan 2024)
--- index.js
+++ index.js
@@ -1,32 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("http://localhost:3000/cities")
+  fetch('http://localhost:3000/cities')
     .then(res => res.json())
-    .then(data => 
-      eventListeners(data))
-    
-    // .then(data => console.log(data))
+    .then(cities => eventListeners(cities))
 });
 
 //create a function to attach event listeners to each button 
-function eventListeners(data){
-  const buttons = document.querySelectorAll('button[id^="button"]') //fancy way of grabbing all buttons with an ID that starts with the word buttongit 
+function eventListeners(cities){
+  const buttons = document.querySelectorAll('button')
     buttons.forEach((button, index) => {
-      button.addEventListener('click', () => displayCityInfo(data[index]))
-      button.addEventListener('mouseover', () => button.style.backgroundColor = 'red')
+      button.addEventListener('click', () => displayCityInfo(cities[index]))
+      button.addEventListener('mouseover', () => button.style.backgroundColor = 'green')
       button.addEventListener('mouseout', () => button.style.backgroundColor = '')
   })
 }
 
 function displayCityInfo(city){
+console.log(city)
 //declare currents card elements
 const locationElement = document.getElementById("location")
 const tempElement = document.getElementById("temperature")
 const windElement = document.getElementById("windspeed")
 const weatherIcon = document.getElementById("weatherIcon")
-
-//set size of weather icon so it fits currents card
-// weatherIcon.style.width = "75px"
-// weatherIcon.style.height = "75px"
 
 //use set of ternary operators to distinguish cities
 locationElement.textContent =
@@ -35,14 +29,9 @@ city.id === 2 ? "Colorado Springs" :
 city.id === 3 ? "Pueblo" :
 city.id === 4 ? "Fort Collins" :
 city.id === 5 ? "Grand Junction" : ""
-  
-//clear out previous city temp wind and wx icon
-tempElement.textContent = ""
-windElement.textContent = ""
-weatherIcon.src = ""
 
 //declare currents
-const currentTemp = parseInt(((city.current.temperature_2m * (9/5)) + 32))
+const currentTemp = parseInt(((city.current.temperature_2m) * 9/5) + 32)
 const currentWind = parseInt(city.current.wind_speed_10m/1.609)
 const currentWeather = city.current.weather_code
 
@@ -65,6 +54,12 @@ else if ([71, 73, 75, 85, 86].includes(currentWeather)){
 else if ([95, 96, 99].includes(currentWeather)){
   weatherIcon.src = "https://i.ibb.co/89FPXgr/lightning-and-rainy-weather-16465.png"
 }
+
+
+
+//clear out previous city temp wind and wx icon
+tempElement.textContent = ""
+windElement.textContent = ""
 
 //setting temp and wind text content display on currents card
 tempElement.textContent = `${currentTemp}°`
